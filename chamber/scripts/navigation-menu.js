1b747609ddccbda6fe4b0fdfaa4f441628381781
jose1905 <davarela231@gmail.com>
2023-01-11 23:04:22 -0600

    Index assignment

---
+++ chamber/scripts/navigation-menu.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('navigation-menu');
+const menu = document.getElementById('links');
+
+button.addEventListener('click', function (){
+    menu.setAttribute(display) = inline;
+})
