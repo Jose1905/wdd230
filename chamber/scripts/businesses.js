bff63f90a0ac95d432db904e0e0272c457a4eba3
jose1905 <davarela231@gmail.com>
2023-03-06 22:04:59 -0600

    Directory incomplete

--- chamber/scripts/businesses.js
+++ chamber/scripts/businesses.js
@@ -3,8 +3,38 @@ const url = "json/data.json"
 async function getBusinessData () {
     const response = await fetch(url);
     const data = await response.json();
-    
-    console.table(data.businesses);
+    displayBusinesses(data.businesses);
+}
+
+const displayBusinesses = (businesses) => {
+    const cards = document.querySelector('div.cards');
+
+    businesses.foreach((business) => {
+        let card = document.createElement('section');
+        let picture = document.createElement('img');
+        let address = document.createElement('h3');
+        let phone = document.createElement('h3');
+        let website = document.createElement('a');
+
+        picture.setAttribute('src', business.imageurl);
+        picture.setAttribute('alt', `Logo of ${business.name}`);
+        picture.setAttribute('loading', 'lazy');
+        picture.setAttribute('width', '340');
+        picture.setAttribute('height', '440');
+
+        address.textContent = business.address;
+
+        phone.textContent = business.phone;
+
+        website.textContent = business.website;
+
+        card.appendChild(picture);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        cards.appendChild(card);
+    })
 }
 
 getBusinessData();
